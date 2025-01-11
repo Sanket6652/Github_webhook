9c86242b475216f97ab761b62a85665034dd3fcc
Sanket6652 <sanketnannaware96@gmail.com>
2025-01-11 23:59:25 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("Connected to MongoDB"))
+  .then(() => console.log("Connected  to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
 // Models
